eb5b5d8b1eea379fd8a4e21fba097bc7bff32025
stolbivi <viktor.stolbin@gmail.com>
2022-05-11 22:06:12 -0400

    Exporting DynamicUI

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { injectIframe, injectScript } from './chrome/Injections';
 import { MessageError, Messages } from './chrome/Messages';
 import { Storage } from './chrome/Storage';
 import { Tabs } from './chrome/Tabs';
+import { DynamicUI } from './core/DynamicUI';
 
 export { injectScript, injectIframe };
 export { Messages, MessageError };
 export { Storage };
 export { Tabs };
+export { DynamicUI };
